7b615e6b499f4b45f087e7fde5a8ef2d7f968052
northtruths <zp1716120350@gmail.com>
2026-03-20 23:32:01 +0800

    简单模拟进程池（任务，未完成）

--- 26_3_19/ProcessPool.cpp
+++ 26_3_19/ProcessPool.cpp
@@ -4,20 +4,33 @@
 #include <unistd.h>      
 #include <sys/types.h> 
 #include<cstdlib>
+#include<sys/wait.h>
+#include<functional>
+#include<time>
+#include"Task.hpp"
 
 class Channel{
 public:
-    Channel();
+    Channel(const int fd)
+        :_wfd(fd)
+    {
+        _name = "Channel_" + std::to_string(_wfd);
+    }
 
     ~Channel();
 private:
-    int _num;
+    int _wfd;//父进程所管理的某管道的写fd
+    std::string _name;//管道名字，用wfd区分
 };
 
+
+
+ 
 int main(int argc, char* argv[]){
     if(argc != 2)
         return 1;//参数无效
     
+    //1.初始化进程池
     int num = std::stoi(argv[1]);
     std::vector<Channel> chas;
     while(num--){
@@ -28,11 +41,29 @@ int main(int argc, char* argv[]){
         int pid = ::fork();
         if(pid == 0){
             close(pipefd[1]);//子进程读，关闭写端
-            //执行任务
+            while(true){
+                int n = 0;
+                read(pipefd[0], &n, sizeof(n));
+                tm[n];
+            }
             exit(0);
         }
-
+        chas.emplace_back(pipefd[1]); 
         close(pipefd[0]);//父进程写，关闭读端
     }
+
+    //2.派发任务
+    int count = 0;//派发任务次数
+    srand((unsigned)time(NULL));
+    while(true){
+        write(chas[count++%num], srand(), sizeof(srand()));
+        sleep(1);
+        if(count == 20)
+            break;
+    }
+
+    int status = 0;
+    ::waitpid(-1, &status, 0);
+    //3.关闭进程池
     return 0;
 }
