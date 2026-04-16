c41ce6670f5dc47e15678b57015685f9205c13e1
northtruths <zp1716120350@gmail.com>
2026-04-16 10:39:52 +0800

    修复回收线程失败的bug

--- 26_4_15/Tickets.cc
+++ 26_4_15/Tickets.cc
@@ -8,12 +8,12 @@
 #define INIT_TICKETS 20 //初始票数
 #define PTHREAD_SIZE 5  //线程数
 
-int num = INIT_TICKETS;//派发数量
+int nums = INIT_TICKETS;//派发数量
 
 class Tickets{
 public:
     Tickets()
-        :_num(0)
+        :_nums(0)
     {
         pthread_create(&_pt, nullptr, grap_tickets, this);
     }
@@ -22,10 +22,10 @@ public:
     static void* grap_tickets(void* p){
         Tickets* t = static_cast<Tickets*>(p);
         t->init_name();
-        while(num > 0){
+        while(nums > 0){
             sleep(1);//增加超卖概率
-            --num;
-            ++t->_num;
+            --nums;
+            ++t->_nums;
             sleep(1);
         }
         return nullptr;
@@ -35,28 +35,30 @@ public:
         _name = "t" + std::to_string(pthread_self());
     }
 
-    void how_many(){
-        std::cout << _name << "抢到了" << _num << "张票" << std::endl;
+    void how_many() const{
+        std::cout << _name << "抢到了" << _nums << "张票" << std::endl;
     }
 
-    pthread_t getid(){
-        return pthread_self();
+    pthread_t getid() const{
+        return _pt;
     }
 private:
     pthread_t _pt;
     std::string _name;
-    int _num;//抢到的票数
+    int _nums;//抢到的票数
 };
 
 int main(){
     Tickets tickets[PTHREAD_SIZE];
 
-    while(1){
+    while(nums > 0){
         for(int i = 0; i < PTHREAD_SIZE; ++i)
             tickets[i].how_many();
-        std::cout << "还剩" << num << "张" << std::endl;
+        std::cout << "还剩" << nums << "张" << std::endl;
         sleep(1);
     }
+    for(int i = 0; i < PTHREAD_SIZE; ++i)
+            tickets[i].how_many();
     for(auto& t : tickets){
         int n = pthread_join(t.getid(), nullptr);
         if(n != 0){
