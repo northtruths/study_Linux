b5202458fa255410531c0183d73b2dae826a3174
northtruths <zp1716120350@gmail.com>
2026-05-30 13:39:33 +0800

    feat: 封装sockaddr_in，内置主机序列和网络字节序两套返回函数

---
+++ 26_5_28/InetAddr.hpp
@@ -0,0 +1,54 @@
+#pragma once
+//封装sockaddr_in
+
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include<string>
+
+
+class InetAddr{
+    void ip_ntoh(){
+        char ipbuffer[64];
+        ip_ = ::inet_ntop(AF_INET, &addr_.sin_addr, ipbuffer, sizeof(ipbuffer));
+    }
+    void port_ntoh(){
+        port_ = ntohs(addr_.sin_port);
+    }
+
+public:
+    InetAddr(sockaddr_in addr)//客户端
+        :addr_(addr)
+    {
+        ip_ntoh();
+        port_ntoh();
+    }
+
+    InetAddr(uint16_t port)//服务端
+        :port_(port)
+    {
+        addr_.sin_family = AF_INET;
+        addr_.sin_port = htons(port_);
+        addr_.sin_addr.s_addr = INADDR_ANY;
+    }
+    ~InetAddr(){}
+
+    in_addr_t IP_INET(){
+        return inet_addr(ip_.c_str());
+    }
+    std::string IP_HOST(){
+        return ip_;
+    }
+    uint16_t PORT_HOST(){
+        return port_;
+    }
+    in_port_t PORT_INET(){
+        return addr_.sin_port;
+    }
+
+private:
+    sockaddr_in addr_;
+    std::string ip_;
+    uint16_t port_;
+};
