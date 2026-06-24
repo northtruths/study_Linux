4b57c7850f9c26876fa0a201a6c1e473c8886ed7
northtruths <zp1716120350@gmail.com>
2026-06-25 00:27:30 +0800

    feat: 协议化数据的传输与接收（简单的运算协议）

--- 26_5_31/TcpClient.hpp
+++ 26_5_31/TcpClient.hpp
@@ -2,58 +2,105 @@
 
 #include "InetAddr.hpp"
 #include "Log.hpp"
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
+#include "protocol.hpp"
 
 using namespace log_module;
+using namespace protocol_module;
 
-class TcpClient{
+bool handle(std::string &msg, std::string &strin)
+{
+    std::stringstream ss(strin);
+    std::string sx, sy, sopt;
+    ss >> sx >> sopt >> sy;
+
+    if (sopt.size() > 1)
+        return false;
+    // stoi转化非法数据会崩溃，所以捕获异常，但先不写
+    int x = std::stoi(sx);
+    int y = std::stoi(sy);
+    char opt = sopt[0];
+    Request rq(x, y, opt);
+    rq.serialize(msg);
+    Pack(msg);
+
+    return true;
+}
+
+class TcpClient
+{
 public:
     TcpClient(std::string ip, uint16_t port)
-        :server_addr_(ip, port)
-    {}
-    ~TcpClient(){}
+        : server_addr_(ip, port)
+    {
+    }
+    ~TcpClient() {}
 
-    void init(){
+    void init()
+    {
         server_fd_ = socket(AF_INET, SOCK_STREAM, 0);
-        if(server_fd_ < 0){
+        if (server_fd_ < 0)
+        {
             LOG(LogLevel::FATAL) << "socket failure";
         }
 
-        int n = connect(server_fd_, (sockaddr*)&server_addr_.sockaddr(), sizeof(server_addr_.sockaddr()));
-        if(n < 0){
+        int n = connect(server_fd_, (sockaddr *)&server_addr_.sockaddr(), sizeof(server_addr_.sockaddr()));
+        if (n < 0)
+        {
             LOG(LogLevel::ERROR) << "connect failure";
             running_ = false;
         }
-        else{
+        else
+        {
             LOG(LogLevel::INFO) << "connect succes... server_fd: " << server_fd_;
         }
     }
 
-    void start(){
-        while(running_){
-            //获取用户输入
-            std::string strsend;
-            std::cout << "请输入: ";
-            std::getline(std::cin, strsend);
-            strsend += '\n';
-            //发送数据
-            send(server_fd_, strsend.c_str(), strsend.size(), 0);
-            //接收返回数据
-            handle();
+    void start()
+    {
+        while (running_)
+        {
+            // 获取用户输入
+            std::string strin;
+            std::cout << "请输入: [x] [opt] [y]" << std::endl;
+            std::getline(std::cin, strin);
+            // 发送数据
+            std::string msg;
+            if (handle(msg, strin) == false)
+            {
+                continue;
+            }
+            send(server_fd_, msg.c_str(), msg.size(), 0);
+            // 接收解析返回数据
+            parse();
         }
     }
 
-    void handle(){
-        char buff[1024];
-        int n = recv(server_fd_, buff, sizeof(buff), 0);
-        if(n < 0){
+    void parse()
+    {
+        char temp[1024];
+        int n = recv(server_fd_, temp, sizeof(temp), 0);
+        if (n < 0)
+        {
             LOG(LogLevel::ERROR) << "recv failure";
         }
-        buff[n] = 0;
-
-        std::cout << buff;
+        temp[n] = 0;
+        std::string buffer = temp;
+        while (!buffer.empty())
+        {
+            Response rs;
+            std::string msg;
+            if(!UnPack(msg, buffer)){
+                break;
+            }
+            rs.deserialize(msg);
+            int state = rs.get_state();
+            int result = rs.get_result();
+            std::cout << "state:" << state << " " << "result:" << result << std::endl;
+        }
     }
+
 private:
     InetAddr server_addr_;
 
