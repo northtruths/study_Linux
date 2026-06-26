e2401a936fea0df15a58ef738fc6562837ed339f
northtruths <zp1716120350@gmail.com>
2026-06-26 22:19:51 +0800

    feat: TcpServer底层模块，用于监听和连接，将IO和业务交给上层

---
+++ 26_6_25/TcpServer.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <functional>
+#include "InetAddr.hpp"
+#include "Socket.hpp"
+#include "Log.hpp"
+
+using namespace log_module;
+
+using Task = std::function<void (const SockPtr&, const InetAddr&)>;
+
+class TcpServer
+{
+public:
+    TcpServer(Task task)
+        : running_(false), task_(task)
+    {
+        listen_socket_.build_tcpserver();
+    }
+
+    void start()
+    {
+        running_ = true;
+        while (running_)
+        {
+            InetAddr client_addr;
+            auto client_socket = listen_socket_.accept_socket(client_addr);
+            if(client_socket == nullptr){
+                continue;
+            }
+            pid_t n = ::fork();
+            if(n < 0){
+                LOG(LogLevel::ERROR) << "fork 失败";
+                return;
+            }else if(n == 0){
+                //子进程
+                listen_socket_.close_socket();
+                if(::fork()){
+                    ::exit(0);
+                }
+                //分离了孙子孤儿进程，可以单独进行IO和业务处理，不阻塞父进程
+                task_(client_socket, client_addr);
+                ::exit(0);
+            }
+            ::waitpid(n, nullptr, 0);
+        }
+    }
+
+    ~TcpServer() {}
+
+private:
+    TcpSocket listen_socket_;
+    bool running_;
+
+    Task task_;
+};
