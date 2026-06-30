a1b183003799b5967d6ec3d561b9714ca3ba27a0
northtruths <zp1716120350@gmail.com>
2026-06-30 16:17:37 +0800

    refactor(TcpServer.hpp): 提供init函数，使其可以延迟传入任务函数task

--- 26_6_25/TcpServer.hpp
+++ 26_6_25/TcpServer.hpp
@@ -10,15 +10,20 @@
 
 using namespace log_module;
 
-using Task = std::function<void (const SockPtr&, const InetAddr&)>;
+using Task = std::function<void(const SockPtr &, const InetAddr &)>;
 
 class TcpServer
 {
 public:
-    TcpServer(Task task, uint16_t port)
-        : running_(false), task_(task)
+    TcpServer(uint16_t port)
+        : running_(false), port_(port)
     {
-        listen_socket_.build_tcpserver(port);
+    }
+
+    void init(Task task)
+    {
+        task_ = task;
+        listen_socket_.build_tcpserver(port_);
     }
 
     void start()
@@ -28,20 +33,25 @@ public:
         {
             InetAddr client_addr;
             auto client_socket = listen_socket_.accept_socket(client_addr);
-            if(client_socket == nullptr){
+            if (client_socket == nullptr)
+            {
                 continue;
             }
             pid_t n = ::fork();
-            if(n < 0){
+            if (n < 0)
+            {
                 LOG(LogLevel::ERROR) << "fork 失败";
                 return;
-            }else if(n == 0){
-                //子进程
+            }
+            else if (n == 0)
+            {
+                // 子进程
                 listen_socket_.close_socket();
-                if(::fork()){
+                if (::fork())
+                {
                     ::exit(0);
                 }
-                //分离了孙子孤儿进程，可以单独进行IO和业务处理，不阻塞父进程
+                // 分离了孙子孤儿进程，可以单独进行IO和业务处理，不阻塞父进程
                 task_(client_socket, client_addr);
                 ::exit(0);
             }
@@ -54,6 +64,6 @@ public:
 private:
     TcpSocket listen_socket_;
     bool running_;
-
+    uint16_t port_;
     Task task_;
 };
